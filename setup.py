cc3c3943790cdacce504e723f9648141c5150f1f
Naoaki Obiki <obiki.wk@gmail.com>
2017-10-17 16:25:54 +0900

    init.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+from pip_github_test import __author__, __version__, __license__
+
+setup(
+        name             = 'gaccho-twitter',
+        version          = __version__,
+        description      = 'Gaccho Twitter Plugin',
+        license          = __license__,
+        author           = __author__,
+        author_email     = 'test@example.com',
+        url              = 'https://github.com/nobiki/gaccho-twitter.git',
+        keywords         = 'gaccho twitter',
+        packages         = find_packages(),
+        install_requires = [],
+        )
+
