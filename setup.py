04bbf6219eb45894210929fb0381db0f7f9452d0
Naoaki Obiki <obiki.wk@gmail.com>
2017-10-17 16:28:34 +0900

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 from setuptools import setup, find_packages
-from pip_github_test import __author__, __version__, __license__
+from gaccho-twitter import __author__, __version__, __license__
 
 setup(
         name             = 'gaccho-twitter',
