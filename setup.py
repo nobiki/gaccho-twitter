04a3416d93d2b21985d299fbb7092eb772c5f5a1
Naoaki Obiki <obiki.wk@gmail.com>
2017-10-17 16:31:04 +0900

    rename gaccho-twitter -> gaccho_twitter

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 from setuptools import setup, find_packages
-from gaccho-twitter import __author__, __version__, __license__
+from gaccho_twitter import __author__, __version__, __license__
 
 setup(
-        name             = 'gaccho-twitter',
+        name             = 'gaccho_twitter',
         version          = __version__,
         description      = 'Gaccho Twitter Plugin',
         license          = __license__,
         author           = __author__,
         author_email     = 'test@example.com',
-        url              = 'https://github.com/nobiki/gaccho-twitter.git',
+        url              = 'https://github.com/nobiki/gaccho_twitter.git',
         keywords         = 'gaccho twitter',
         packages         = find_packages(),
         install_requires = [],
