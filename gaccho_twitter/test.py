91a548e2947b76f45042d8c62749703a3db00591
Naoaki Obiki <obiki.wk@gmail.com>
2017-10-18 15:18:33 +0900

    modified:   gaccho_twitter/test.py

--- gaccho_twitter/test.py
+++ gaccho_twitter/test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+import pkg_resources
 
 def foo():
-    print('foo')
+    for dist in pkg_resources.working_set:
+        print(dist.project_name, dist.version)
