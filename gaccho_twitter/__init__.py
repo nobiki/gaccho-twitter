857b3b67676b9243ddc6a7a53283a4969a5ede3a
Naoaki Obiki <obiki.wk@gmail.com>
2017-10-18 14:53:53 +0900

    test

--- gaccho_twitter/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-__author__  = 'nobiki'
-__version__ = '0.0.1'
-__license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from gaccho_twitter import __author__, __version__, __license__
 
 setup(
         name             = 'gaccho_twitter',
-        version          = __version__,
+        version          = '0.0.1',
         description      = 'Gaccho Twitter Plugin',
-        license          = __license__,
-        author           = __author__,
+        license          = 'MIT',
+        author           = 'nobiki',
         author_email     = 'test@example.com',
         url              = 'https://github.com/nobiki/gaccho_twitter.git',
         keywords         = 'gaccho twitter',
