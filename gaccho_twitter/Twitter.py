33d9ae899bdc01644d2d25fe6a8cd791f4e521c7
Naoaki Obiki <obiki.wk@gmail.com>
2018-01-18 13:54:21 +0900

    modified:   gaccho_twitter/Twitter.py

--- gaccho_twitter/Twitter.py
+++ gaccho_twitter/Twitter.py
@@ -4,7 +4,7 @@ import os
 import time
 import configparser
 
-from twitter import Twitter, Oauth, read_token_file
+from twitter import Twitter, OAuth, read_token_file
 from twitter.api import Api
 
 # load config
